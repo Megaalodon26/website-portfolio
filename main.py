e97e1ab4adc125c238c886d9a0b0ee701d2ad505
meg <megan_lynn@comcast.com>
2025-01-07 13:56:20 -0500

    Add description on the new columns

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="centered")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    with Image.open('images/pfp2-2025.png') as img:
-        # Rotate the image by 270 degrees
-        rotated_img = img.rotate(270, expand=True)
-        # Save the rotated image to a temporary file
-        rotated_img.save('images/rotated_pfp2-2025.png')
-    # Display the rotated image in Streamlit
     st.image('images/rotated_pfp2-2025.png')
 
 with col2:
     st.title("Megan Lynn")
     content = """
-    Hi! I'm Megan! I'm a self-taught software engineer and I love to build things. Here's an intro at my personal website and projects I've worked on.
+    Hi! I'm Megan! I'm a self-taught software engineer and I love to build things. I'm passionate about Philadelphia sports, Star Wars/Marvel movies, and
+     Nintendo video games. Here's an intro at my personal website and projects I've worked on.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I've built with Python. Feel free to check them out and contact me!
+"""
+st.write(content2)
